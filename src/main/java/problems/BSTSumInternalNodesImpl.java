ff25b859c8821990cd54e875d169d7b78e4eeab6
Alisson Amarante <alisson.amarante@ccc.ufcg.edu.br>
2025-04-09 00:20:23 -0300

    soma nós internos

--- src/main/java/problems/BSTSumInternalNodesImpl.java
+++ src/main/java/problems/BSTSumInternalNodesImpl.java
@@ -1,10 +1,24 @@
 package problems;
 
 import adt.bst.BST;
+import adt.bt.BTNode;
 
 public class BSTSumInternalNodesImpl implements BSTSumInternalNodes{
     public int sumInternalNodes(BST<Integer> bst){
-        //TODO implement your code here
-        throw new UnsupportedOperationException("Not implemented yet!");
+       int sum = 0;
+       if (!bst.getRoot().isEmpty()) {
+        sum = sumNode(bst.getRoot(), 0);
+       }
+       return sum;
+    }
+
+    private int sumNode(BTNode<Integer> node, int soma) {
+        int result = soma;
+        if (!node.isEmpty() && node.getParent() != null && !node.isLeaf()) {
+            result += node.getData();            
+        }
+        result = sumNode(node.getLeft(), result);
+        result = sumNode(node.getRight(), result);
+        return result;
     }
 }
